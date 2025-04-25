4ae8a008f31400ede347a10c9ae46662d5860ba2
Olivia Vittenet <olivia.vittenet@isen.yncrea.fr>
2025-04-25 15:46:58 +0200

    gitignore db.sqlite3

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -7,7 +7,6 @@ describe('Create and connect to an account', () => {
   it('Visits the OC commerce site and creates an account', () => {
     cy.visit('/home')
 
-    // Création de compte
     cy.contains('SIGNUP').click()
     cy.url().should('include', '/user/signup')
 
@@ -20,7 +19,6 @@ describe('Create and connect to an account', () => {
     cy.get('form').contains('Register').click()
     cy.url().should('include', '/user/login')
 
-    // Connexion
     cy.get('[id^=your_name]').type(username)
     cy.get('[id^=your_pass]').type(password)
     cy.get('form').contains('Log in').click()
@@ -31,7 +29,7 @@ describe('Create and connect to an account', () => {
 
 describe('Put item in favourite', () => {
   it('Connect to OC commerce and manage favourite products', () => {
-    // Connexion
+    
     cy.visit('/home')
     cy.contains('LOGIN').click()
     cy.url().should('include', '/user/login')
@@ -42,26 +40,20 @@ describe('Put item in favourite', () => {
     cy.url().should('include', '/home')
     cy.contains('FAVOURITE')
 
-    // Aller dans la page FAVOURITE et vérifier qu’elle est vide
     cy.contains('FAVOURITE').click()
     cy.url().should('include', '/favourite')
     cy.contains('No Product in your favourite list').should('exist')
 
-    // Revenir à l'accueil
     cy.visit('/home')
 
-    // Ajouter un produit aux favoris
     cy.get('[id^=favBtn]').first().click()
 
-    // Vérifier qu’il est bien dans les favoris
     cy.contains('FAVOURITE').click()
     cy.url().should('include', '/favourite')
     cy.get('table tbody tr').should('have.length.at.least', 1)
 
-    // Supprimer le produit
     cy.get('[id^=favBtn]').first().click()
 
-    // Vérifier que la liste est vide
     cy.reload()
     cy.contains('No Product in your favourite list').should('exist')
   })
@@ -71,25 +63,19 @@ describe('Dark Mode Toggle Button', () => {
   it('should toggle dark and light themes and persist preference', () => {
     cy.visit('/home')
 
-    // Vérifie que le bouton existe
     cy.get('#theme-toggle').should('exist').and('be.visible')
 
-    // Vérifie que le mode clair est actif au départ
     cy.get('body').should('not.have.class', 'bg-dark')
 
-    // Clique sur le bouton pour activer le mode sombre
     cy.get('#theme-toggle').click()
 
-    // Vérifie que le mode sombre est activé
     cy.get('body').should('have.class', 'bg-dark')
     cy.get('body').should('have.class', 'text-white')
 
-    // Recharge la page et vérifie que le mode sombre est conservé
     cy.reload()
     cy.get('body').should('have.class', 'bg-dark')
     cy.get('body').should('have.class', 'text-white')
 
-    // Rebasculer en mode clair
     cy.get('#theme-toggle').click()
     cy.get('body').should('not.have.class', 'bg-dark')
   })
